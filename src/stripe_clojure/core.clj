da75437d45f98bf012731e540888b742e1a24d90
Prachetas Prabhu <prachetas.github@gmail.com>
2014-10-24 06:27:13 +0000

    Major simplification approaching initial release

--- src/stripe_clojure/core.clj
+++ src/stripe_clojure/core.clj
@@ -1,15 +1,28 @@
-;   Copyright (c) 2014 Alberto Bengoa/Prachetas Prabhu. All rights reserved.
-;   The use and distribution terms for this software are covered by the
-;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
-;   which can be found in the file epl-v10.html at the root of this distribution.
-;   By using this software in any fashion, you are agreeing to be bound by
-;   the terms of this license.
-;   You must not remove this notice, or any other, from this software.
-
 (ns stripe-clojure.core
   "Functions for Stripe Customers API"
-  (:require [stripe-clojure.http-util :refer [do-request set-tokens!]]
-            [clj-http.client :only [get post delete] :as client]))
+  (:require [clj-http.client :as client])
+  (:refer-clojure :exclude (list)))
+
+;; TODO: add support for public token selection?
+(def stripe-tokens (atom {:public "" :private ""}))
+(defn set-tokens! [m] (swap! stripe-tokens (fn [a] (merge a m))))
+
+(def stripe-api-url "https://api.stripe.com/v1/")
+(defn build-url [[url-ks url-vs] params]
+  (str stripe-api-url
+    (apply str
+      (interpose "/"
+        (filter (comp not nil?)
+          (interleave url-ks (map params url-vs)))))))
+
+(defn make-request
+  [params method resource]
+  (:body (method (build-url resource params)
+           {:basic-auth [(:private @stripe-tokens)]
+            :query-params (apply dissoc params (second resource))
+            :throw-exceptions false
+            :as :json
+            :coerce :always})))
 
 (def url-vals {"cards" :card_id
                ;;"charges" :charge_id
@@ -22,47 +35,42 @@
                "subscriptions" :subscription_id
                "tokens" :token_id})
 
-(defn build-url-map-vals [resources]
-  [resources (map url-vals resources)])
-
 ;; resources lacking test coverage are commented out
-;; contains a map like {:cards [["customers" "cards"] [:customer_id :card_id]]...}
+;; map like {:cards [["customers" "cards"] [:customer_id :card_id]]...}
+;; second vector values can also be nil
 (def url-mapping (into {}
-                   (map (fn [[k v]] [k (build-url-map-vals v)])
+                   (map (fn [[k v]] [k [v (map url-vals v)]])
                      {:cards ["customers" "cards"]
                       ;;:charges ["charges"]
-                      ;;:charges-capture ["charges" "capture"] ; no-id endpt
-                      ;;:charges-refund ["charges" "refund"] ; no-id endpt
+                      ;;:capture ["charges" "capture"] ; no-id endpt
+                      ;;:refund ["charges" "refund"] ; no-id endpt
                       ;;:coupons ["coupons"]
                       :customers ["customers"]
                       ;;:discounts ["customers" "discount"] ; no-id endpt
                       :events ["events"]
                       ;;:invoiceitems ["invoiceitems"]
                       :invoices ["invoices"]
-                      ;;:invoices-lines ["invoices" "lines"] ; no-id endpt
-                      ;;:invoices-pay ["invoices" "pay"] ; no id endpt
-                      ;;:invoices-upcoming ["invoices" "upcoming"] ; no-id endpt
+                      ;;:lines ["invoices" "lines"] ; no-id endpt
+                      ;;:pay ["invoices" "pay"] ; no id endpt
+                      ;;:upcoming ["invoices" "upcoming"] ; no-id endpt
                       :plans ["plans"]
                       :subscriptions ["customers" "subscriptions"]
                       :tokens ["tokens"]})))
 
-;; TODO: add params validation e.g. to make sure a get-list doesn't
-;; contain an id
-(defmacro defop [op-name http-action & {:keys [op]}]
-  `(def ~op-name
-     (fn [resource# params#]
-       (do-request params# ~http-action
-         (url-mapping (if ~op
-                        (-> resource# name (str "-" ~op) keyword)
-                        resource#))))))
+(defmacro defop
+  "Creates function that accepts a map of resource keyword to map of params"
+  [op-name http-action]
+  `(defn ~op-name [resource#]
+     (let [[kw# params#] (first resource#)]
+       (make-request params# ~http-action (url-mapping kw#)))))
 
 ;; operations lacking test coverage are commented out
-(defop stripe-cancel client/delete)
-#_(defop stripe-capture client/post :op "capture")
-(defop stripe-create client/post)
-(defop stripe-delete client/delete)
-(defop stripe-get client/get)
-(defop stripe-list client/get)
-#_(defop stripe-pay client/post :op "pay")
-#_(defop stripe-refund client/post :op "refund")
-(defop stripe-update client/post)
+(defop cancel client/delete)
+#_(defop capture client/post)
+(defop create client/post)
+(defop delete client/delete)
+(defop retrieve client/get)
+(defop list client/get)
+#_(defop pay client/post)
+#_(defop refund client/post)
+(defop update client/post)
--- test/stripe_clojure/test/core.clj
+++ test/stripe_clojure/test/core.clj
@@ -1,80 +1,46 @@
-;   Copyright (c) 2014 Alberto Bengoa/Prachetas Prabhu. All rights reserved.
-;   The use and distribution terms for this software are covered by the
-;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
-;   which can be found in the file epl-v10.html at the root of this distribution.
-;   By using this software in any fashion, you are agreeing to be bound by
-;   the terms of this license.
-;   You must not remove this notice, or any other, from this software.
-
 (ns stripe-clojure.test.core
-  (:use [stripe-clojure core])
-  (:require [stripe-clojure.http-util :refer [set-tokens! name-to-kw]])
-  (:require [stripe-clojure.test.core-config :refer [existing-cust-id secret-tokens test-plan]])
+  (:require [stripe-clojure.core :as s])
+  (:require [stripe-clojure.test.core-config :refer [secret-tokens]])
   (:require [clojure.test :refer [deftest is testing]]))
 
-#_(deftest test-build-path
-  (is (= (build-path :subscriptions) [["/customers" "customers"] ["/subscriptions" "subscriptions"]]) "Building resource path"))
+(deftest test-set-tokens!
+  (testing "set-tokens!"
+    (testing "private and public" (is (= (s/set-tokens! {:private "private" :public "public"}) {:private "private" :public "public"})))
+    (testing "private only" (is (= (s/set-tokens! {:private "privater"}) {:private "privater" :public "public"})))
+    (testing "public only" (is (= (s/set-tokens! {:public "publicer"}) {:private "privater" :public "publicer"})))
+    (testing "empty" (is (= (s/set-tokens! {}) {:private "privater" :public "publicer"})))))
 
-(def crud-params #{:get :create :update :delete :base})
-(def test-card {:number "4242424242424242" :exp_month 12 :exp_year 2020 :cvc 123 :name "Mr. Stripe Clojure"})
-(def test-customer {:card test-card
-                    :email "mrclojure@stripetest.com"
-                    :description "customer test from clj-stripe"})
+(deftest test-build-url
+  (is (= (s/build-url [["customers" "cards"] [:customer_id :card_id]]
+           {:customer_id "PHL123" :card_id "ORD789"})
+        (str s/stripe-api-url "customers/PHL123/cards/ORD789")) "build normal url")
+  (is (= (s/build-url [["charges" "refund"] [:charge_id nil]]
+           {:customer_id "LAX123" :charge_id "JFK789"})
+        (str s/stripe-api-url "charges/JFK789/refund")) "build nil-value url"))
 
-(set-tokens! secret-tokens)
+(deftest test-url-mapping
+  (is (= (:cards s/url-mapping) [["customers" "cards"] [:customer_id :card_id]]) "test url mapping"))
 
-;; Only tests CRUD for now
-;; TODO: Add ability to specify assertion for non-crud operations
-(defmacro deftest-resource [r-kw params]
-  `(let [bps# (merge {} (:base ~params))
-         ;;do-op# (fn [op# r# & ps#] (op# r# (apply merge ps# (:base ~params))))
-         {r-id# :id :as r-result#} (if-let [ps# (:create ~params)]
-                                     (stripe-create ~r-kw (merge ps# bps#))
-                                     (stripe-get ~r-kw (merge (:get ~params) bps#)))
-         r-map# {(name-to-kw (name ~r-kw)) r-id#}
-         ;;non-crud-ops# (select-keys ~params (filter #(not (contains? crud-params %)) (keys ~params)))
-         ]
-     (testing "create (or first get for non-create resources)" (is (not (nil? r-id#))))
-     (testing "get") (is (= (stripe-get ~r-kw (merge r-map# bps#)) r-result#))
-     (when-let [ps# (:update ~params)]
-       (testing "update" (is (= (stripe-update ~r-kw (merge r-map# ps# bps#)) (stripe-get ~r-kw (merge r-map# bps#))))))
-     #_(doseq [[op# op-params#] non-crud-ops#]
-         (op# ~r-kw (merge r-map# op-params# bps#)))
-     (if (:delete ~params)
-       ((:delete ~params) ~r-kw (merge r-map# bps#))
-       (testing "delete/cancel" (is (every? false? (map #(= r-id# (:id %)) (:data (stripe-list ~r-kw bps#)))))))))
+(def card {:number "4242424242424242"
+           :exp_month 12
+           :exp_year 2020
+           :cvc 123
+           :name "Mr. Stripe Clojure"})
+(def customer {:card card
+               :email "mrclojure@stripetest.com"
+               :description "customer test from clj-stripe"})
 
-(deftest cards-test
-  (testing "Cards"
-      (deftest-resource :cards
-        {:base {:customer_id existing-cust-id}
-         :create {:card test-card}
-         :update {:name "Mr. NewStripe Clojure"}
-         :delete stripe-delete})))
-
-(deftest card-tokens-test
-  (testing "Card Tokens"
-    (let [{token-id :id :as create-token-result} (stripe-create :tokens {:card test-card})]
-      (testing "create" (is (not (nil? token-id))))
-      (testing "get" (is (= (stripe-get :tokens {:token_id token-id}) create-token-result))))))
-
-(deftest customers-test
-  (testing "Customers"
-    (deftest-resource :customers
-      {:create test-customer
-       :update {:email "newmrclojure@stripetest.com"}
-       :delete stripe-cancel})))
-
-(deftest events-test
-  (testing "Events"
-    (let [{event-id :id :as recent-event} (-> (stripe-get :events {}) :data first)]
-      (testing "list" (is (not (nil? event-id))))
-      (testing "get" (is (= (stripe-get :events {:event_id event-id}) recent-event))))))
-
-(deftest subscriptions-test
-  (testing "Subscriptions"
-    (deftest-resource :subscriptions
-      {:create {:plan test-plan}
-       :base {:customer_id existing-cust-id}
-       :update {:quantity 2}
-       :delete stripe-delete})))
+(s/set-tokens! secret-tokens)
+(deftest functional-test
+  (testing "Functional Test -"
+    (let [{id :id :as c} (s/create {:customers customer})
+          c-id {:customer_id id}]
+      (testing "create" (is (not (nil? (:id c)))))
+      (testing "retrieve" (is (= (s/retrieve {:customers c-id}) c)))
+      (testing "update" (is (= (s/update {:customers
+                                          (merge c-id {:email "newmrclojure@stripetest.com"})})
+                              (s/retrieve {:customers c-id}))))
+      (s/delete {:customers c-id})
+      (testing "delete and list" (is (every? false?
+                                       (map #(= id (:id %))
+                                                      (:data (s/list {:customers {}})))))))))
